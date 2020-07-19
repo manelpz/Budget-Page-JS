5a29eb9df57101abe53137f99492e14f729be996
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-19 17:00:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 
 var budgetController = (function(){
-    var x = 23;
-    
-    var add = function(a){
-        return x+a;
-    }
-
-   
+  
 })();
 
 var UIController = (function(){
 
+    var DOMstrings = {
+        inputType :'.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    };
+
+
+    return {
+        getInput:function(){
+            return{
+                type:document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        }, 
+        getDOMstring:function(){
+            return DOMstrings;
+        }
+    };
 })();
 
 
-var controller = (function(budgetController,UIController){
+var controller = (function(budgetCtrl,UICtrl){
+    var DOM = UIController.getDOMstring();
+    var ctrlAddItem = function(){
+
+        var input = UIController.getInput();
+        console.log(input);
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
