b252e7915395a77ac17fcebdee1486ee11bddec6
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-18 23:35:56 -0500

    creation of Modules

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var budgetController = (function(){
+    var x = 23;
+    
+    var add = function(a){
+        return x+a;
+    }
+
+   
+})();
+
+var UIController = (function(){
+
+})();
+
+
+var controller = (function(budgetController,UIController){
+
+})(budgetController, UIController);
