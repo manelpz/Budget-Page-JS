e0ea7d8bc6129b769838535faa37abac22965966
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-22 01:11:57 -0500

    adding function constructors

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 
 var budgetController = (function(){
-  
+  var Expense = function(id,description,value){
+      this.id = id;
+      this.description = description;
+      this.value = value;
+  };
+
+  var Income = function(id,description,value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+       allItems:{
+        exp: [],
+        inc:[]
+       },
+       totals:{
+           exp:0,
+           inc:0
+       }
+  }
+
 })();
 
 var UIController = (function(){
@@ -43,7 +65,6 @@ var controller = (function(budgetCtrl,UICtrl){
     };
  
     var ctrlAddItem = function(){
-
         var input = UICtrl.getInput();
         //console.log(input);
     };
