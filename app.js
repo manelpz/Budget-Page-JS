b469e33c5c95c5b72f21572582ec8213cd1875de
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-28 21:38:46 -0500

    replacing html

--- app.js
+++ app.js
@@ -67,14 +67,15 @@ var UIController = (function(){
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         }, 
+
         addListItem: function(obj, type){
-            var html;
+            var html, newHtml;
             
             if(type === "inc"){
-                html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div>< class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="income-0"><div class="item__description">%description%</div>< class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             else if (type =="exp" ){
-                html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="expense-0"><div class="item__description">%%description</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
                 }
             },
         getDOMstring:function(){
