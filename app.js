05c20577e679c9106a2f651018a4c9cb6f74ec63
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-31 01:08:36 -0500

    avoiding null values

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ var UIController = (function(){
             return{
                 type:document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         }, 
 
@@ -121,15 +121,27 @@ var controller = (function(budgetCtrl,UICtrl){
             }
         });
     };
+
+    var updateBudget = function() {
+
+    }
  
     var ctrlAddItem = function(){
         var input, newItem;
         input = UICtrl.getInput();
         //console.log(input);
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        UICtrl.addListItem(newItem, input.type);
 
-        UICtrl.clearFields();
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            UICtrl.addListItem(newItem, input.type);
+    
+            UICtrl.clearFields();
+
+            updateBudget();
+        }
+
+        
+
     };
 
     return {
