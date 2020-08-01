0a22f34d1d554ed45961f474857d7bd111a98b6c
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-31 21:38:24 -0500

    adding displayBudget

--- app.js
+++ app.js
@@ -89,7 +89,11 @@ var UIController = (function(){
         inputValue : '.add__value',
         inputBtn : '.add__btn',
         incomeContainer : '.income__list',
-        expenseContainer : '.expenses__list'
+        expenseContainer : '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
 
@@ -133,9 +137,16 @@ var UIController = (function(){
 
             fieldsArr[0].focus();
         },
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        },
         getDOMstring:function(){
             return DOMstrings;
         }
+
     };
 })();
 
@@ -158,7 +169,7 @@ var controller = (function(budgetCtrl,UICtrl){
 
         budgetCtrl.calculateBudget();
         var budget = budgetCtrl.getBudget();
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     }
  
     var ctrlAddItem = function(){
