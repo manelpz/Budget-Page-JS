e1c9bc4f85d119dc52c980e3905622b306b349ad
Emmanuel López Guerrero <manelpz@icloud.com>
2020-08-01 20:47:28 -0500

    add delete item

--- app.js
+++ app.js
@@ -93,7 +93,8 @@ var UIController = (function(){
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
 
 
@@ -169,6 +170,8 @@ var controller = (function(budgetCtrl,UICtrl){
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
     var updateBudget = function() {
@@ -191,9 +194,12 @@ var controller = (function(budgetCtrl,UICtrl){
 
             updateBudget();
         }
+    };
+    
+    var ctrlDeleteItem = function(event){
 
+    console.log(event.target);
         
-
     };
 
     return {
