bff4d1308474f51cc8dc6ee0e77f926c3c5e7db0
Emmanuel López Guerrero <manelpz@icloud.com>
2020-08-08 17:40:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -55,16 +55,15 @@ var budgetController = (function(){
     deleteItem: function(type, id){
         var ids, index;
 
-        ids = data.allItems[type].map(function(current){s
-            return current.id;
-        });
+      ids = data.allItems[type].map(function(current){
+        return current.id;
+      });
 
-        index = ids.indexOf(id);
+      index = ids.indexOf(id);
 
-        if(index !=== -1){
-            data.allItems[type].splice(index,1);
-
-        }
+      if(index !== -1){
+          data.allItems[type].splice(index,1);
+      }
     },
 
     calculateBudget: function(){
